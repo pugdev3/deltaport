d72f2ae23a03135856955f143b52b0e2c224c350
pugdev3 <pugdev@duck.com>
2026-08-22 12:37:00 +0100

    feat: Sincronizar com branch principal

--- DELTARUNE.sh
+++ DELTARUNE.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+ARG=$1
 DELTARUNEDIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 DELTARUNEPID=""
 SAVEDIR="$HOME/.config/DELTARUNE"
@@ -31,33 +32,10 @@ if [ -f "$DELTARUNEDIR/.ubuntu" ]; then
 fi
 
 # Checar se arquivos de gatilho estão aqui por algum motivo (???) e deletar eles
-if [ -f "$CHAPTERSELECT_FILE" ]; then
-	rm $CHAPTERSELECT_FILE
-fi
-
-if [ -f "$CHAPTER1_FILE" ]; then
-	rm $CHAPTER1_FILE
-fi
-
-if [ -f "$CHAPTER2_FILE" ]; then
-	rm $CHAPTER2_FILE
-fi
-
-if [ -f "$CHAPTER3_FILE" ]; then
-	rm $CHAPTER3_FILE
-fi
-
-if [ -f "$CHAPTER4_FILE" ]; then
-	rm $CHAPTER4_FILE
-fi
-
-if [ -f "$CHAPTER5_FILE" ]; then
-	rm $CHAPTER5_FILE
-fi
-
-if [ -f "$END_FILE" ]; then
-	rm $END_FILE
-fi
+TRIGGER_FILES=("$CHAPTERSELECT_FILE" "$CHAPTER1_FILE" "$CHAPTER2_FILE" "$CHAPTER3_FILE" "$CHAPTER4_FILE" "$CHAPTER5_FILE")
+for f in "${TRIGGER_FILES[@]}"; do
+	[ -n "$f" ] && [ -f "$f" ] && rm "$f"
+done
 
 if [ -f "$DELTARUNEDIR/.saida_lock" ]; then
 	rm "$DELTARUNEDIR/.saida_lock"
@@ -119,57 +97,45 @@ function run_game {
 	GAME_WATCH=1
 }
 
-# Rodar esse jogaço!!!
-cd "$DELTARUNEDIR"
-run_game
-
 # =- Toda a lógica para mudar de capitulos / Processar arquivos de gatilho -=
 parse_file() {
 	cd "$SAVEDIR"
-    if [ "$1" == "$CHAPTERSELECT_FILE" ]; then
-		rm $1
-		change_chapter 0
-	elif [ "$1" == "$CHAPTER1_FILE" ]; then
-		rm $1
-		change_chapter 1
-	elif [ "$1" == "$CHAPTER2_FILE" ]; then
-		rm $1
-		change_chapter 2
-    	elif [ "$1" == "$CHAPTER3_FILE" ]; then
-		rm $1
-		change_chapter 3
-	elif [ "$1" == "$CHAPTER4_FILE" ]; then
-		rm $1
-		change_chapter 4
-	elif [ "$1" == "$CHAPTER5_FILE" ]; then
-		rm $1
-		change_chapter 5
-	else
-		return
-	fi
+	local chapter=""
+	case "$1" in
+		"$CHAPTERSELECT_FILE") chapter=0 ;;
+		"$CHAPTER1_FILE")      chapter=1 ;;
+		"$CHAPTER2_FILE")      chapter=2 ;;
+		"$CHAPTER3_FILE")      chapter=3 ;;
+		"$CHAPTER4_FILE")      chapter=4 ;;
+		"$CHAPTER5_FILE")      chapter=5 ;;
+		*) return ;;
+	esac
+	rm "$1"
+	change_chapter "$chapter"
 }
 
 change_chapter() {
-	if [ "$1" == "0" ]; then
-		cd "$DELTARUNEDIR"
-		run_game
-	elif [ "$1" == "1" ]; then
-		cd "$DELTARUNEDIR/chapter1_linux"
-		run_game
-	elif [ "$1" == "2" ]; then
-		cd "$DELTARUNEDIR/chapter2_linux"
-		run_game
-	elif [ "$1" == "3" ]; then
-		cd "$DELTARUNEDIR/chapter3_linux"
-		run_game
-	elif [ "$1" == "4" ]; then
-		cd "$DELTARUNEDIR/chapter4_linux"
-		run_game
-	elif [ "$1" == "5" ]; then
-		cd "$DELTARUNEDIR/chapter5_linux"
-		run_game
-	fi
+	local target="$DELTARUNEDIR"
+	case "$1" in
+		0) target="$DELTARUNEDIR" ;;
+		1) target="$DELTARUNEDIR/chapter1_linux" ;;
+		2) target="$DELTARUNEDIR/chapter2_linux" ;;
+		3) target="$DELTARUNEDIR/chapter3_linux" ;;
+		4) target="$DELTARUNEDIR/chapter4_linux" ;;
+		5) target="$DELTARUNEDIR/chapter5_linux" ;;
+		*) return ;;
+	esac
+	cd "$target"
+	run_game
 }
+
+# Rodar esse jogaço!!!
+cd "$DELTARUNEDIR"
+if [[ "$ARG" != "" ]]; then
+	change_chapter $1
+else
+	run_game
+fi
 
 # Monitorar pasta de save para arquivos de gatilho
 inotifywait -m $SAVEDIR  |
